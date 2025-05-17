a561a3dd5007a200689c6b708d490cf07a7515fd
Sandhya ojha <sandhyaojha@resiliencesoft.com>
2025-05-18 00:42:56 +0530

    fix:name

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World-with Hrkirat!');
   });
 // Error handler
 // app.use(errorMiddleware);
